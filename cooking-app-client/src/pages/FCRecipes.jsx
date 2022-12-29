18fa643438ab30ef4a0effd469e0f4eb7f2faf0d
assafhan <83879496+assafhan@users.noreply.github.com>
2022-12-29 19:53:24 +0200

    FCrecipes update

--- cooking-app-client/src/pages/FCRecipes.jsx
+++ cooking-app-client/src/pages/FCRecipes.jsx
@@ -1,3 +1,21 @@
+import { Component } from "react";
+import { useContext, useEffect, useState } from 'react';
+import axios from "axios";
 export default function FCRecipes() {
-    return <div>recipes</div>;
+    const apiUrl = "https://localhost:44347/api/ingredients";
+
+
+    useEffect(() => {
+        axios.get(apiUrl).then((res) => console.log(res.data))
+    }, [])
+
+
+
+    return <div>recipes
+        <div></div> <br />
+
+    </div>;
+
+
+
 }
