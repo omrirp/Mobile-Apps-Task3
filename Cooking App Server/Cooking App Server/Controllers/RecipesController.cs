7e65c26e1f99a47fd6551ea2c30e8937e56dc4c7
omrirp <omrirp@gmail.com>
2022-12-30 16:44:44 +0200

    insert recipe and ings in recipe

---
+++ Cooking App Server/Cooking App Server/Controllers/RecipesController.cs
@@ -0,0 +1,41 @@
+﻿using Cooking_App_Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Cooking_App_Server.Controllers
+{
+    public class RecipesController : ApiController
+    {
+        // GET: api/Recipes
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Recipes/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Recipes
+        public string Post([FromBody] Recipe r, [FromBody]  List<int> ingIds)
+        {
+            return r.addRecipe(ingIds);
+        }
+
+        // PUT: api/Recipes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Recipes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Cooking App Server/Cooking App Server/Models/Recipe.cs
@@ -0,0 +1,48 @@
+﻿using Cooking_App_Server.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cooking_App_Server.Models
+{
+    public class Recipe
+    {
+        int id;
+        string name;
+        string imageURL;
+        string cookingMethod;
+        float time;
+
+        public Recipe() { }
+
+        public Recipe(string name, string imageURL, string cookingMethod, float time)
+        {
+            Name = name;
+            ImageURL = imageURL;
+            CookingMethod = cookingMethod;
+            Time = time;
+        }
+
+        public Recipe(int id, string name, string imageURL, string cookingMethod, float time)
+        {
+            Id = id;
+            Name = name;
+            ImageURL = imageURL;
+            CookingMethod = cookingMethod;
+            Time = time;
+        }
+
+        internal string addRecipe(List<int> ingIds)
+        {
+            DBservices ds = new DBservices();
+            return ds.insertRecipe(this, ingIds);
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string ImageURL { get => imageURL; set => imageURL = value; }
+        public string CookingMethod { get => cookingMethod; set => cookingMethod = value; }
+        public float Time { get => time; set => time = value; }
+    }
+}
